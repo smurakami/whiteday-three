ffa455cb073da5e0705c282e822b350ff3d70c6c
s.murakami <smurakami.84@gmail.com>
2019-03-14 07:26:40 +0900

    commit for deploy

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+git add .
+git cm -m 'commit for deploy'
+
 git push origin master
 ssh smurakami.com "cd /var/www/apps/whiteday/; git pull origin master"
 # scp -r . smurakami.com:/var/www/apps/whiteday
